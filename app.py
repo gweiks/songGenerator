cbeb58f48ef27f2a2581eca9d11b853860830578
George <bolden@weiksner.com>
2026-01-04 15:00:06 -0800

    Built python functions to scrape songs, train the model, and generate random songs

--- app.py
+++ app.py
@@ -1,11 +1,141 @@
 from flask import Flask, send_file
 import os
+import time
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+import random
+import time
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return send_file('home.html')
+
+def scrape_lyrics():
+    """
+    Scrapes the lyrics from the webpage and organizes them into a dataframe.
+    Args:
+        None
+    Returns:
+        df: pandas dataframe with the title and lyrics
+    """
+
+    songs = []
+    url = "https://www.streetdirectory.com/lyricadvisor/artist/cclco/maroon_5/"
+
+    #scrape the songs from the webpage
+    for i in range(1,4):
+        response = requests.get(url + str(i) + "/")
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        for a_tag in soup.find_all("a", class_="tahoma13purplebold"):
+            href = a_tag.get("href")
+
+            if href and "lyricadvisor/song" in href:
+                songs.append(href)
+
+        time.sleep(5)
+
+    song_lyrics = []
+    song_names = []
+
+    #organize the lyric data into a dataframe (title and lyrics)
+    for song in songs:
+        response = requests.get(song)
+
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        lyrics_div = soup.find("div", id="content_lyric_clean")
+
+        if lyrics_div != None:
+            lyrics = "\n".join(lyrics_div.stripped_strings).replace("\u2019", "")
+            lyrics = lyrics.encode("latin1").decode("utf-8").replace("[CHORUS]", " ")
+            song_lyrics.append(lyrics)
+
+        song_name = soup.find("a", class_ = "tahoma33purple").text.replace(" Lyrics", "")
+        song_names.append(song_name)
+        time.sleep(5)
+
+    df = pd.DataFrame({"Title": song_names, "Lyrics": song_lyrics})
+    df.to_csv('maroon_5_lyrics.csv', index=False)
+    return df
+
+
+def train_model(df):
+    """
+    Trains the model on the lyrics.
+    Args:
+        df: pandas dataframe with the lyrics
+    Returns:
+        chain: dictionary of the words and the words that follow them
+    """
+
+    lyrics = df["Lyrics"].tolist()
+    all_lyrics = []
+
+    for lyric in lyrics:
+        text = lyric.lower()
+        text = re.sub(r'[^a-z\s]', '', text)
+        lines = text.split('\n')
+
+        #add the start and end tokens to the lyrics so we know where to start and end the song
+        lines[0] = "<START> " + lines[0]
+        lines[-1] = lines[-1] + " <END>"
+
+        all_lyrics.append(lines)
+    
+    #create a dictionary of the words and the words that follow them
+    chain = {"<START>": []}
+
+    for lyric in lines:
+        words_in_song = []
+
+        for i in range(len(lyric)):
+            words = (lyric[i]).split()
+
+            if i > 0:
+                words.insert(0, "<N>")
+
+            for word in words:
+                words_in_song.append(word)
+
+        for j in range(len(words_in_song) - 1):
+            #if the word is not in the dictionary, add it
+            if words_in_song[j] not in chain:
+                chain[words_in_song[j]] = []
+
+            #add the word and the word that follows it to the dictionary
+            chain[words_in_song[j]].append(words_in_song[j + 1])
+
+    return chain
+
+
+def generate_song(chain):
+    """
+    Args:
+        - chain: a dict representing a unigram Markov chain
+
+    Returns:
+        A string representing the randomly generated song.
+    """
+    #Initialize the words list with the first word (randomly chosen from the start token)
+    words = [random.choice(chain["<START>"])]
+
+    #Generate the next word, appending to the words list
+    current_word = words[0]
+
+    #Generate the next word until the end token is reached
+    while (current_word != "<END>"):
+        next_word = random.choice(chain[current_word])
+        words.append(next_word)
+        current_word = next_word
+
+    # join the words together into a string with line breaks
+    lyrics = " ".join(words[:-1])
+    return "\n".join(lyrics.split("<N>"))
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
